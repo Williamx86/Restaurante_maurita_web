fbde5354f89a2083967d0940f706210e4a7bef4d
William Barahona <wbarahonacastillo00118818@gmail.com>
2020-12-18 10:56:12 -0600

    Archivos validos

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const x = document.getElementById("boton_menu");
-const y = document.getElementById("lat");
-
-x.addEventListener('click',function(){
-    y.classList.toggle("oculto");
-    y.style.width = "35%";
-})
