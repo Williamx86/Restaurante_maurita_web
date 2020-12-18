6968d38ee61673fe05370995ceb00aefe6214b14
William Barahona <wbarahonacastillo00118818@gmail.com>
2020-12-18 16:30:27 -0600

    Pagina de incio finalizada

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,6 +2,22 @@ const x = document.getElementById("boton_menu");
 const y = document.getElementById("lat");
 
 x.addEventListener('click',function(){
-    y.classList.toggle("oculto");
-    y.style.width = "35%";
+    if(window.innerWidth <= 750){
+        y.classList.toggle("oculto");
+        y.style.width = "60%";
+    }else{
+        y.classList.toggle("oculto");
+        y.style.width = "35%";
+    }
 })
+
+function cambiar(z){
+    const imagen = document.getElementById("imagen");
+    if(z==1){
+        imagen.src = "src/img/picture1.jpg";
+    }else if(z==2){
+        imagen.src = "src/img/picture2.jpg";
+    }else if(z==3){
+        imagen.src = "src/img/picture3.jpg";
+    }
+}
